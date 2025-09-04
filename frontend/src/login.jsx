1864f7fbd059ce7ef9c6731828c697456cc93d96
edaescob <edaescob@nequi.com>
2025-09-04 14:36:16 -0500

    cambio logo

--- frontend/src/login.jsx
+++ frontend/src/login.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import api, { AUTH_PATH } from "./utils/api";
-import logo from "./assets/logo_nomina";
+import logo from "./assets/logo_nomina.png";
 
 const Login = ({ onLoginSuccess }) => {
   const [email, setEmail] = useState("");
